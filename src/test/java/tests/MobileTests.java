0346b23240cb5be42b2903e1caf4cfa1ae790962
RyazanovNY <riazanovnikita@gmail.com>
2025-12-14 18:40:49 +0300

    Edited classes after feedback

--- src/test/java/tests/MobileTests.java
+++ src/test/java/tests/MobileTests.java
@@ -1,32 +1,22 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import screens.SearchScreen;
 
 import static io.qameta.allure.Allure.step;
 
-
+@DisplayName("Класс для проверки функциональности поиска в интернет-энциклопедии «Википедия»")
 public class MobileTests extends TestBase {
 
     SearchScreen searchScreen = new SearchScreen();
 
     @Test
-    @DisplayName("Успешный поиск по слову «Appium»")
-    void successfulSearchTest() {
-        step("Ввести «Appium» в поисковую строку", () -> {
-            searchScreen
-                    .clickOnSearchTab()
-                    .insertSearchText("Appium");
-        });
-
-        step("Проверить найденный контент", () -> {
-            searchScreen
-                    .checkSearchResultForSpecificText("Appium");
-        });
-    }
-
-    @Test
+    @Feature("Функциональность поиска в интернет-энциклопедии")
+    @Story("Я как пользователь хочу иметь возможность пользоваться строкой поиска без ошибок")
+    @Owner("NikitaRyazanov")
+    @Severity(SeverityLevel.BLOCKER)
     @DisplayName("Проверка функциональности поиска")
     void emptySearchTest() {
         step("Ввести «» в поисковую строку", () -> {
@@ -38,6 +28,25 @@ public class MobileTests extends TestBase {
         step("Проверка страницу с результатом поиска «Страница с поиском не пустая", () -> {
             searchScreen
                     .checkIfPageListIsNotEmpty();
+        });
+    }
+
+    @Test
+    @Feature("Функциональность поиска в интернет-энциклопедии")
+    @Story("Я как пользователь хочу иметь возможность выполнять поиск по конкретному слову")
+    @Owner("NikitaRyazanov")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("Успешный поиск по слову «Appium»")
+    void successfulSearchTest() {
+        step("Ввести «Appium» в поисковую строку", () -> {
+            searchScreen
+                    .clickOnSearchTab()
+                    .insertSearchText("Appium");
+        });
+
+        step("Проверить найденный контент", () -> {
+            searchScreen
+                    .checkSearchResultForSpecificText("Appium");
         });
     }
 }
